68e1cc89ffb328a7ef7d8cd45bcbac6aa82c1a6d
Luke Spivak <luke.spivak@edu.sait.ca>
2026-08-13 14:16:36 -0600

    Update checkout URLs to use HTTPS for improved security

--- frontend/src/pages/ClubNoblesPage.jsx
+++ frontend/src/pages/ClubNoblesPage.jsx
@@ -31,8 +31,8 @@ async function openCheckoutPage() {
       },
     ],
     mode: 'payment',
-    success_url: 'http://white-cliff-0d1d7a20f.7.azurestaticapps.net/club-nobles/checkout/success',
-    cancel_url: 'http://white-cliff-0d1d7a20f.7.azurestaticapps.net/club-nobles',
+    success_url: 'https://white-cliff-0d1d7a20f.7.azurestaticapps.net/club-nobles/checkout/success',
+    cancel_url: 'https://white-cliff-0d1d7a20f.7.azurestaticapps.net/club-nobles',
   });
   window.location.replace(session.url);
 }
